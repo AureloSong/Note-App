771cbc3d0107574ce890f4223170f61dec8bc1d6
AureloSong <fnat.123@gmail.com>
2025-07-22 17:13:50 -0300

    Merge branch 'main' of

--- frontend/note-app/src/pages/Login/Login.jsx
+++ frontend/note-app/src/pages/Login/Login.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate} from 'react-router-dom';
 import PasswordInput from '../../components/Input/PasswordInput';
 import { validateEmail } from '../../utils/helper';
+import axiosInstance from '../../utils/axiosInstance';
 const Login = () => {
 
 const [email, setEmail] =useState("");
 const [password, setPassword] =useState("");
 const [error, setError] =useState(null);
-
+const navigate = useNavigate();
 const handleLogin = async (e) => {
   e.preventDefault();
 
@@ -25,6 +26,26 @@ const handleLogin = async (e) => {
   setError("")
 
   //Login API Call
+
+  try {
+    const response = await axiosInstance.post("/login", {
+      email: email,
+      password: password
+    })
+
+    if (response.data && response.data.accessToken) {
+      localStorage.setItem("accessToken", response.data.accessToken);
+      navigate('/dashboard');
+    }
+  } catch (error) {
+    if(error.response && error.response.data) {
+      setError(error.response.data.message || "Login failed. Please try again.");
+    }
+    else {
+      setError("An unexpected error occurred. Please try again.");
+    }
+  }
+
 }
 
 return  (
